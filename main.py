87efa37d8461a679b09f1d66a37cc10162ee4b63
teachmain <teachmain@outlook.com>
2024-03-04 10:58:01 +0800

    update

--- main.py
+++ main.py
@@ -1,28 +1,156 @@
 import serial
+import time
+import math
+import matplotlib.pyplot as plot
+import numpy
+import pyautogui
 port = serial.Serial('/dev/ttyUSB0',115200)
-def readFrame()->bytearray:
-    frame = bytearray()
+def readSection()->bytearray:
+    section = bytearray()
     while True:
         if port.read()[0] == 0xAA:
             if port.read()[0] == 0x55:
-                frame.append(0xAA)
-                frame.append(0x55)
-                frame.append(port.read()[0])
+                section.append(0xAA)
+                section.append(0x55)
+                section.append(port.read()[0])
                 lsn = port.read()[0]
-                frame.append(lsn)
+                section.append(lsn)
                 remain_byte_number = 6 + 2*lsn
-                print(remain_byte_number)
                 for i in range(remain_byte_number):
-                    frame.append(port.read()[0])
-                return frame
+                    section.append(port.read()[0])
+                return section
     
 
 def print_hex(bytes):
   l = [hex(int(i)) for i in bytes]
   print(" ".join(l))
 
-frame = readFrame()
-print_hex(frame)
-frame = readFrame()
-print_hex(frame)
+def is_header(section)->bool:
+    if section[2] & 1 == 1:
+        return True
+    else:
+        return False
+
+def readFrame(buff):
+    frame = []
+    if len(buff) !=0 :
+        frame.append(buff.pop(0))
+    else:
+        temp_section = readSection()
+        while not is_header(temp_section):
+            temp_section = readSection()
+        frame.append(temp_section)
+    
+    temp_section = readSection()
+    while not is_header(temp_section):
+        frame.append(temp_section)
+        temp_section = readSection()
+    
+    buff.append(temp_section)
+    return frame
+
+def decodeSection(section):
+    pointNum = section[3]
+    startAngle = (section[4] + section[5] * 256.0) / 128.0
+    endAngle = (section[6] + section[7] * 256.0) / 128.0
+    while endAngle < startAngle:
+        endAngle += 360
+    diffAngle = endAngle - startAngle
+    if pointNum > 1 :
+        stepAngle = diffAngle / (pointNum-1)
+    else:
+        stepAngle = 0
+    result = []
+    for i in range(pointNum):
+        if section[10+2*i] & 3 > 1:
+            continue
+        else:
+            distance = (section[10+2*i] >> 2) + section[11+2*i] * 64
+            angle = startAngle + i*stepAngle
+
+            if distance != 0:
+                angle += math.atan(21.8 * (155.3 - distance) / (155.3 * distance))
+
+            angle = -angle
+            angle += 90
+
+            while angle<0 or angle >= 360:
+                if angle >= 360 :
+                    angle -= 360
+
+                if angle < 0:
+                    angle += 360
+
+            angle /= 180
+            angle *= math.pi
+            x = math.cos(angle) * distance
+            y = math.sin(angle) * distance
+            result.append([x,y])
+    return result
+
+def decodeFrame(frame):
+    result = []
+    for section in frame:
+        section_result = decodeSection(section)
+        result += section_result
+    return result
+
+def combine(points,ref_distance):
+    result = []
+    flag = 0
+    sumx=0
+    sumy=0
+    count=0
+    for point in points:
+        temp_x = point[0]
+        temp_y = point[1]
+        if count > 0:
+            avg_x = sumx/count
+            avg_y = sumy/count
+            temp_dist = math.sqrt((avg_x - temp_x)**2 + (avg_y-temp_y)**2)
+            if temp_dist > ref_distance:
+                result.append([avg_x,avg_y])
+                sumx = temp_x
+                sumy = temp_y
+                count = 1
+        else:
+                sumx = temp_x
+                sumy = temp_y
+                count = 1
+    if count != 0:
+        avg_x = sumx/count
+        avg_y = sumy/count
+        result.append([avg_x,avg_y])
+    return result
+
+
+
+
+        
+        
+
+
+plot.ion()
+fig,ax = plot.subplots()
+while True:
+    buff = []
+    frame = readFrame(buff)
+    result = decodeFrame(frame)
+    points = numpy.array(result)
+    if len(points) == 0:
+        continue
+    points = combine(points,100)
+    points = numpy.array(points)
+    print(points.shape)
+    points = points.T
+    plot.cla()
+    ax.set_xlim(-500,500)
+    ax.set_ylim(-500,500)
+    plot.scatter(points[0],points[1])
+    plot.pause(0.001)
+    #print(pyautogui.position())
+    
+    
+    
+
 
